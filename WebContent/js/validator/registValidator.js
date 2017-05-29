edd509a5c1df5529209f36e4e4a0f011c69e6780
shuffle <282650205@qq.com>
2017-05-29 23:11:29 +0800

    fix bug in the validator

--- WebContent/js/validator/registValidator.js
+++ WebContent/js/validator/registValidator.js
@@ -14,24 +14,9 @@ $(document).ready(function() {/* 文档加载，执行一个函数*/
 			message: '用户名不能为空'
 		    },
 		    stringLength: {
-			min: 6,
+			min: 5,
 			max: 15,
-			message: '用户名长度必须在6到15之间'
-		    },
-		    threshold :  6 , //有6字符以上才发送ajax请求，（input中输入一个字符，插件会向服务器发送一次，设置限制，6字符以上才开始）
-		    remote: {//ajax验证。server result:{"valid",true or false} 向服务发送当前input name值，获得一个json数据。例表示正确：{"valid",true}  
-			url: 'exist2.do',//验证地址
-			message: '用户已存在',//提示消息
-			delay :  2000,//每输入一个字符，就发ajax请求，服务器压力还是太大，设置2秒发送一次ajax（默认输入一个字符，提交一次，服务器压力太大）
-			type: 'POST'//请求方式
-			    /**自定义提交数据，默认值提交当前input value
-			     *  data: function(validator) {
-                               return {
-                                   password: $('[name="passwordNameAttributeInYourForm"]').val(),
-                                   whatever: $('[name="whateverNameAttributeInYourForm"]').val()
-                               };
-                            }
-			     */
+			message: '用户名长度必须在5到15之间'
 		    },
 		    regexp: {
 			regexp: /^[a-zA-Z0-9_\.]+$/,
